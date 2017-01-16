b5da36cae25632872e988d1342cfbbbf44281c15
Loveorigami <loveorigami@mail.ru>
2017-01-16 18:15:44 +0300

    yt as closure

--- src/web/youtube/YoutubeShortcodes.php
+++ src/web/youtube/YoutubeShortcodes.php
@@ -7,7 +7,7 @@ use yii\helpers\Html;
 /**
  * Plugin Name: Youtube Video
  * Plugin URI: https://github.com/loveorigami/yii2-shortcodes-pack/tree/master/src/web/youtube
- * Version: 1.3
+ * Version: 1.4
  * Description: A shortcode for embed youtube video in view. Use as [youtube code="ZM2tVuy8B_Y"]
  * Author: Andrey Lukyanov
  * Author URI: https://github.com/loveorigami
@@ -21,26 +21,23 @@ class YoutubeShortcodes extends BaseShortcode
     {
         return [
             // show original link
-            'yt' => [
-                'tooltip' => '[yt code="*"]...[/yt]',
-                'config' => [
-                    'code' => 'ZM2tVuy8B_Y',
-                ],
-                'callback' => function ($attrs, $content, $tag) {
-                    $title = $content ? $content : 'shortcode ' . $tag;
+            'yt' => function ($attrs, $content, $tag) {
+                $title = $content ? $content : 'shortcode ' . $tag;
+                if (isset($attrs['code'])) {
                     return Html::a($title, 'https://www.youtube.com/embed/' . $attrs['code'], ['target' => '_blank']);
-                },
-            ],
+                }
+                return null;
+            },
             // embed video
             'youtube' => [
-                'tooltip' => '[youtube code="*" w="*" h="*"]',
+                'callback' => [YoutubeWidget::class, 'widget'],
                 'config' => [
                     'code' => 'ZM2tVuy8B_Y',
                     'w' => 560,
                     'h' => 315,
                     'controls' => 2
                 ],
-                'callback' => [YoutubeWidget::class, 'widget'],
+                'tooltip' => '[youtube code="*" w="*" h="*"]'
             ]
         ];
     }

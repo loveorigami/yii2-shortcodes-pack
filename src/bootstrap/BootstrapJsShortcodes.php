8e277d95245bc2471bd88513fe91badce607fbf8
Loveorigami <loveorigami@mail.ru>
2017-01-19 11:33:37 +0300

    tabs shortcode

---
+++ src/bootstrap/BootstrapJsShortcodes.php
@@ -0,0 +1,33 @@
+<?php
+namespace lo\shortcodes\bootstrap;
+
+use lo\plugins\BaseShortcode;
+use lo\shortcodes\bootstrap\widgets\Tabs;
+
+/**
+ * Plugin Name: Bootstrap 3 JavaScript Shortcodes
+ * Plugin URI: https://github.com/loveorigami/yii2-shortcodes-pack/tree/master/src/bootstrap
+ * Version: 1.0
+ * Description: A shortcodes pack with Bootstrap 3 JavaScript
+ * Author: Andrey Lukyanov
+ * Author URI: https://github.com/loveorigami
+ */
+class BootstrapJsShortcodes extends BaseShortcode
+{
+    /**
+     * @return array
+     */
+    public static function shortcodes()
+    {
+        return [
+            'tabs' => [
+                'callback' => [Tabs::class, 'widget'],
+                'config' => [
+                    'close' => false
+                ],
+                'tooltip' => '[tabs][tab] ... [/tab][/tabs]'
+            ],
+        ];
+    }
+}
+
---
+++ src/bootstrap/widgets/Tabs.php
@@ -0,0 +1,57 @@
+<?php
+namespace lo\shortcodes\bootstrap\widgets;
+
+use lo\plugins\helpers\ShortcodesHelper;
+use yii\bootstrap\Tabs as BootstrapTabs;
+use yii\helpers\ArrayHelper;
+
+/**
+ * Class Tabs
+ * @package lo\shortcodes\bootstrap\widgets
+ * @author Lukyanov Andrey <loveorigami@mail.ru>
+ */
+class Tabs extends BootstrapWidget
+{
+    /**
+     * @var array
+     */
+    protected $items;
+
+    /**
+     * @return string
+     */
+    public function run()
+    {
+        $this->getItemsFromContent();
+        return BootstrapTabs::widget([
+            'items' => $this->items
+        ]);
+    }
+
+    /**
+     * populate $items
+     */
+    protected function getItemsFromContent()
+    {
+        $pattern = ShortcodesHelper::shortcodeRegex('tab');
+        preg_replace_callback($pattern, [$this, 'doShortcodeTag'], $this->content);
+    }
+
+    /**
+     * @param $m
+     */
+    protected function doShortcodeTag($m)
+    {
+        $tag = $m[2];
+        $attr = ShortcodesHelper::parseAttributes($m[3]);
+        $content = isset($m[5]) ? $m[5] : null;
+
+        $index = count($this->items);
+
+        $this->items[$index] = [
+            'content' => $content,
+            'label' => ArrayHelper::getValue($attr, 'label', $tag.'-'.$index),
+            'active' => ArrayHelper::getValue($attr, 'active') ? true : false
+        ];
+    }
+}
